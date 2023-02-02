bdc1ad6f371f7b2462ab18a323c5cfc2741a2f66
Juan Ramirez <rami8224.2007@gmail.com>
2023-02-02 15:44:34 -0500

    updated Details

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Icon } from "@iconify/react";
-// import angularIcon from "@iconify/icons-logos/angular-icon";
+import awsIcon from "@iconify/icons-logos/aws";
 import reactIcon from "@iconify/icons-logos/react";
 // import vueIcon from "@iconify/icons-logos/vue";
+// import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 class About extends Component {
   render() {
@@ -30,10 +31,10 @@ class About extends Component {
                     src={profilepic}
                     alt="Avatar placeholder"
                   />
-                  {/* <Icon
-                    icon={angularIcon}
+                  <Icon
+                    icon={awsIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  /> */}
+                  />
                   <Icon
                     icon={reactIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
